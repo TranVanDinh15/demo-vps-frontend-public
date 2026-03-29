7d26e13d2743ecaebd1cb6754a265765be38de71
TranVanDinh15 <vandinh410807@gmail.com>
2026-03-30 06:55:51 +0700

    first-auto-build

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ function HomePage() {
       {/* Hero Section */}
       <section className="hero">
         <h1 className="hero-title">
-          Full-Stack App
+          Full-Stack App DinhPro@BugTaiToi
           <span className="hero-highlight"> Laravel + React + MongoDB</span>
         </h1>
         <p className="hero-subtitle">
